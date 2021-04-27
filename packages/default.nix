ae6313bdf8d8b5bb1971836a18f2c10d5fc23fab
John Mager <jmageriii@gmail.com>
2021-04-27 15:51:27 -0400

    Switch to TOML for standard packages

--- packages/default.nix
+++ packages/default.nix
@@ -6,29 +6,46 @@ let
 
   callPackage = file: args: pkgs.callPackage file (lib.recursiveUpdate { inherit buildIdris; } args);
 
-in
-rec {
-  inherit callPackage;
+  callTOML = file:
+    let
+      defaults = {
+        version = "0.0";
+        depends.idrisDeps = [ ];
+        depends.extraDeps = [ ];
+      };
+      tomlPackage = lib.recursiveUpdate defaults
+        (builtins.fromTOML (builtins.readFile file));
+      toIdrisPackage = toml: {
+        inherit (toml) name version;
+        src = fetchFromGitHub toml.source;
+        extraBuildInputs = builtins.map (p: pkgs.${p}) toml.depends.extraDeps;
+        idrisLibraries = builtins.map (p: packages.${p}) toml.depends.idrisDeps;
+      };
+    in
+    buildIdris (toIdrisPackage tomlPackage);
 
   packages = rec {
 
     idris2api = callPackage ./idris2api.nix { inherit idris2; };
 
-    comonad = callPackage ./comonad.nix { };
-
-    elab-util = callPackage ./elab-util.nix { };
-
-    sop = callPackage ./sop.nix { inherit elab-util; };
-
-    pretty-show = callPackage ./pretty-show.nix { inherit elab-util sop; };
-
-    hedgehog = callPackage ./hedgehog.nix { inherit elab-util sop pretty-show; };
-
     readline-sample = callPackage ./readline-sample.nix { };
 
-    lsp = callPackage ./lsp.nix { inherit idris2api; };
+    comonad = callTOML ./comonad.toml;
+
+    elab-util = callTOML ./elab-util.toml;
+
+    sop = callTOML ./sop.toml;
+
+    pretty-show = callTOML ./pretty-show.toml;
+
+    hedgehog = callTOML ./hedgehog.toml;
 
   };
+
+
+in
+{
+  inherit callPackage packages;
 
   withPackages = fn: with-packages (fn packages);
 }
