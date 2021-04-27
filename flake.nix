830de329bbe7d68dedb7c8638e1002ceaf17eac6
John Mager <jmageriii@gmail.com>
2021-04-27 17:37:31 -0400

    [ refactor ] Use overlay in flake.nix

--- flake.nix
+++ flake.nix
@@ -9,29 +9,28 @@
   inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = { self, nixpkgs, idris2-src, flake-utils }:
-    # Without the racket backend, we can't build on ARM yet.
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "i686-linux" ]
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          idris2 = pkgs.callPackage ./idris2 { inherit idris2-src; };
-        in
-        {
-          packages.idris2 = idris2;
-
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nixpkgs-fmt
-            ];
-          };
-
-        }
-      ) // {
-
+    {
       overlay = final: prev: {
         idris2 = prev.callPackage ./idris2 { inherit idris2-src; };
       };
 
       templates = import ./templates;
-    };
+    } //
+
+    # Without the racket backend, we can't build on ARM yet.
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "i686-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+        in
+        {
+          packages.idris2 = pkgs.idris2;
+
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.nixpkgs-fmt ];
+          };
+
+        }
+      );
+
 }
