6a77e15147b3007de7ef82f7f25728360b7f4ac1
John Mager <jmageriii@gmail.com>
2021-04-30 09:35:28 -0400

    Add toml template

--- templates/default.nix
+++ templates/default.nix
@@ -3,4 +3,8 @@
     path = ./simple;
     description = "A simple Idris2 project";
   };
+  toml = {
+    path = ./toml;
+    description = "A simple Idris2 project with a TOML specification";
+  };
 }
---
+++ templates/toml/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My Idris 2 package";
+
+  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.idris2-pkgs.url = github:claymager/idris2-pkgs;
+
+  outputs = { self, nixpkgs, idris2-pkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ idris2-pkgs.overlay ]; };
+        mypkg = pkgs.idris2.buildTOMLSource ./. ./mypkg.toml;
+
+      in
+      {
+
+        defaultPacakage = mypkg;
+
+        devShell = pkgs.mkShell {
+          buildInputs = mypkg.dev-inputs;
+        };
+
+      }
+    );
+}
