10a6f99b1d9f2fb8a55070fd940f00375a24de63
John Mager <jmageriii@gmail.com>
2021-06-13 18:35:17 -0400

    [ templates ] Be explicit about supported systems
    aarch64 is unsupported for chez, which prevents users from running `nix
    flake check`

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -5,22 +5,20 @@
   inputs.idris2-pkgs.url = github:claymager/idris2-pkgs;
 
   outputs = { self, nixpkgs, idris2-pkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" "i686-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ idris2-pkgs.overlay ]; };
 
-        # Idris2, and the libraries you want available
+        # Idris2, and any libraries you want available
         idris2-with-pkgs = pkgs.idris2.withPackages
           (ps: with ps; [
             idris2api
           ]);
       in
       rec {
-
         devShell = pkgs.mkShell {
           buildInputs = [ idris2-with-pkgs ];
         };
-
       }
     );
 }
--- templates/toml/flake.nix
+++ templates/toml/flake.nix
@@ -5,11 +5,10 @@
   inputs.idris2-pkgs.url = github:claymager/idris2-pkgs;
 
   outputs = { self, nixpkgs, idris2-pkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" "i686-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ idris2-pkgs.overlay ]; };
         mypkg = pkgs.idris2.buildTOMLSource ./. ./mypkg.toml;
-
       in
       {
         defaultPackage = mypkg;
