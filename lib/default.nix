0f9473209cecdb5eac0d249b261f5a0e37ccc4f9
John Mager <jmageriii@gmail.com>
2021-10-20 16:29:50 -0400

    [ lib ] Add myself to maintainers list

--- lib/default.nix
+++ lib/default.nix
@@ -1,16 +1,28 @@
-let attrsets = {
+let
+  attrsets = {
 
-  /* Like builtins.getAttr, but with a default value
+    /* Like builtins.getAttr, but with a default value
 
-    Example:
-    x = { a = 42; }
-    maybeAttr 0 "a" x
-    => 42
-    maybeAttr 0 "b" x
-    => 0
-  */
-  maybeAttr = def: name: attrs:
-    attrs."${name}" or def;
-};
+      Example:
+      x = { a = 42; }
+      maybeAttr 0 "a" x
+      => 42
+      maybeAttr 0 "b" x
+      => 0
+    */
+    maybeAttr = def: name: attrs:
+      attrs."${name}" or def;
+  };
 
-in attrsets // { inherit attrsets; }
+  maintainers = {
+    /* same format as NixOS maintainers-list.nix */
+    claymager = {
+      name = "John Mager";
+      email = "jmageriii@gmail.com";
+      github = "claymager";
+      githubId = 8852150;
+    };
+  };
+
+in
+attrsets // { inherit attrsets maintainers; }
