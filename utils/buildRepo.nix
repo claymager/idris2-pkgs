3708dc4269553e4f3f0124bb60c79f6a0838a0d0
John Mager <jmageriii@gmail.com>
2021-10-21 23:09:33 -0400

    Refactor buildRepo.nix

--- utils/buildRepo.nix
+++ utils/buildRepo.nix
@@ -1,53 +1,61 @@
 { callPackage, buildIdris, lib, renamePkgs, ipkg-to-json }: basePkgs:
-src: { extraPkgs ? { }, ... }@args:
+src: { extraLibs ? { }, ... }@args:
 let
+
   inherit (builtins) match readDir readFile removeAttrs;
-  inherit (lib.lists) any filter findSingle;
+  inherit (lib.lists) any filter flatten findSingle;
   inherit (lib.attrsets) attrNames recursiveUpdate maybeAttr;
+  inherit (lib.strings) hasSuffix;
 
   # ipkgToNix : (contents : String) -> Attrs*
   ipkgToNix = callPackage ./ipkg-to-nix.nix { inherit buildIdris; src = ipkg-to-json; };
 
-  choosePkgs = ps: extraPkgs: depends:
-    let
-      ipkgs = recursiveUpdate ps extraPkgs;
-      savedPkgNames = attrNames extraPkgs;
-      renameDeps = dep: maybeAttr dep.name dep.name (
-        removeAttrs renamePkgs savedPkgNames
-      );
-      depNames = map renameDeps depends ++ [ "prelude" "base" ];
-    in
-    map (d: maybeAttr (throw "Unknown idris package ${d}") d ipkgs) depNames;
-
-
-  pkgmap = choosePkgs basePkgs;
-
   err = msg: throw "When configuring package for ${src}:\n${msg}";
 
   /* Loads data from (what it guesses is) the primary .ipkg */
-  ipkg = args.ipkgFile or (
+  ipkgFile = args.ipkgFile or (
     let
-      allIpkgs = lib.lists.flatten (filter (x: x != null)
+      ipkgFiles = flatten (filter (x: x != null)
         (map (match "(.*)\\.ipkg") (attrNames (readDir src))));
       /* It is common to include  something like `mypkg-docs.ipkg` at the toplevel.
         We want to ignore such a file, unless specified otherwise. */
       ignored = [ "test" "tests" "doc" "docs" ];
-      notIgnored = fn: !any (pat: lib.strings.hasSuffix pat fn) ignored;
+      notIgnored = fn: !any (pat: hasSuffix pat fn) ignored;
       main = findSingle
         notIgnored
         (err "No valid *.ipkg file found")
         (err "Multiple valid *.ipkg files found")
-        allIpkgs;
+        ipkgFiles;
     in
     main + ".ipkg"
   );
 
-  ipkgData = ipkgToNix (readFile (src + "/${ipkg}"));
+  ipkgData = ipkgToNix (readFile (src + "/${ipkgFile}"));
+
+  # chooseFrom : Attrs Packages -> List String -> List Pacakges
+  chooseFrom = ps: depends:
+    let
+      extraPkgs = args.extraPkgs or { };
+      allPkgs = recursiveUpdate ps extraPkgs;
+      savedPkgNames = attrNames extraPkgs;
+
+      /* TODO: renameDeps is just using attrset lookup as a map, where the keys are
+        "idris2 package" names and the values are `idris2-pkgs` names.
+
+        However, unicode compatibility issues are likely here: idris2 supports unicode
+        package names, while nix does not allow unicode in keys.
+      */
+      renameDeps = dep: maybeAttr dep.name dep.name (
+        removeAttrs renamePkgs savedPkgNames
+      );
+      depNames = map renameDeps depends;
+    in
+    map (d: maybeAttr (err "Unknown idris package ${d}") d allPkgs) depNames;
 
 in
 buildIdris ({
   inherit src;
   inherit (ipkgData) name version;
-  idrisLibraries = pkgmap extraPkgs ipkgData.depends;
+  idrisLibraries = chooseFrom basePkgs ipkgData.depends;
   executable = ipkgData.executable or "";
 } // args)
