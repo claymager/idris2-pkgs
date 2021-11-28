62c7ab1dae7c8c5d8501277b930002a6e2a32907
John Mager <jmageriii@gmail.com>
2021-11-28 11:02:37 -0500

    [ template ] Follow main's nixpkgs
    By using the same nixpkgs, an importing flake is (more) able to make
    use of the build cache.

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -1,8 +1,11 @@
 {
   description = "My Idris 2 package";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.idris2-pkgs.url = "github:claymager/idris2-pkgs";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    idris2-pkgs.url = "github:claymager/idris2-pkgs";
+    nixpkgs.follows = "idris2-pkgs/nixpkgs";
+  };
 
   outputs = { self, nixpkgs, idris2-pkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" "i686-linux" ] (system:
