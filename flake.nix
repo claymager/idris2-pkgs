ea364b7b3dbe9a748b284cafac5c8d9d24b1bc4f
rhiannon morris <rhi@rhiannon.website>
2022-10-30 16:15:49 +0100

    update packages
    - remove commit hash from idrall
    - idris-server renamed to recombine
    - `nix flake update`

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     effect = { url = "github:russoul/idris2-effect"; flake = false; };
     hedgehog = { url = "github:stefan-hoeck/idris2-hedgehog"; flake = false; };
     fvect = { url = "github:mattpolzin/idris-fvect"; flake = false; };
-    idrall = { url = "github:alexhumphreys/idrall/13ef174"; flake = false; };
+    idrall = { url = "github:alexhumphreys/idrall"; flake = false; };
     ipkg-to-json = { url = "github:claymager/ipkg-to-json"; flake = false; };
     inigo = { url = "github:idris-community/Inigo"; flake = false; };
     lsp = { url = "github:idris-community/idris2-lsp"; flake = false; };
@@ -29,7 +29,7 @@
     rhone = { url = "github:stefan-hoeck/idris2-rhone"; flake = false; };
     rhone-js = { url = "github:stefan-hoeck/idris2-rhone-js"; flake = false; };
     tailrec = { url = "github:stefan-hoeck/idris2-tailrec"; flake = false; };
-    idris-server = { url = "gitlab:avidela/idris-server"; flake = false; };
+    recombine = { url = "gitlab:avidela/recombine"; flake = false; };
     xml = { url = "github:madman-bob/idris2-xml"; flake = false; };
     indexed = { url = "github:mattpolzin/idris-indexed"; flake = false; };
     hashable = { url = "github:z-snails/idris2-hashable"; flake = false; };
